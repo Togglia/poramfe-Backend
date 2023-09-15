92505e6a493b9812696e40679347a7db31c08d9e
keviness0720 <hsoo0720@naver.com>
2023-09-15 16:00:42 +0900

    first commit

---
+++ src/main/java/com/poramfe/poramfe/PoramfeApplication.java
@@ -0,0 +1,13 @@
+package com.poramfe.poramfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoramfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoramfeApplication.class, args);
+	}
+
+}
