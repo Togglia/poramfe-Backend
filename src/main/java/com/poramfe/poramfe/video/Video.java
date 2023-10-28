bbb869fdfd00bcd9e5273858ecb0998a17fee691
Hyuk Su <hsoo0720@naver.com>
2023-10-28 15:19:50 +0900

    add Video Repository

---
+++ src/main/java/com/poramfe/poramfe/video/Video.java
@@ -0,0 +1,34 @@
+package com.poramfe.poramfe.video;
+
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Video implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private byte[] videoData;
+
+    public void setTitle(String title) {
+    }
+
+    public void setVideoData(byte[] bytes) {
+    }
+
+    public byte[] getVideoContent() {
+        return videoData;
+    }
+
+    public String getFileName() {
+        return title;
+    }
+
+    // 생성자, Getter 및 Setter
+}
+
